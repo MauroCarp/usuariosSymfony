865f3744e1e8d7b8ea4baec40997f424011e30f7
= <=>
2022-06-20 14:11:19 +0200

    Data Table Configurado

--- src/Controller/UsuariosController.php
+++ src/Controller/UsuariosController.php
@@ -43,4 +43,67 @@ class UsuariosController extends AbstractController
     }
 
 
+    /**
+     * @Route("/usuarios/tablaUsuarios", name="tablaUsuarios")
+     */
+
+    public function userList(){
+
+        $em = $this->getDoctrine()->getManager();
+        $usuarios = $em->getRepository(Usuarios::class)->findAll();
+
+        if(count($usuarios) == 0){
+
+            echo '{"data": []}';
+
+            return;
+        }
+
+        $datosJson = '{
+			"data": [';
+
+        foreach ($usuarios as $key => $user) {
+
+            $btns = "<div class='btn-group'><button class='btn btn-warning btnEditarUsuario' 
+            idUsuario='".$user->getId()."' data-toggle='modal' data-target='#modalEditarUsuario' >
+            <i class='fa fa-pencil'></i></button>
+            <button class='btn btn-danger btnEliminarUsuario' idUsuario='".$user->getId()."'>
+            <i class='fa fa-times'></i></button></div>";
+
+
+            $sexo = ($user->getSexo() == 'M') ? 'Masculino' : 'Femenino';
+
+            $estado = 'Baja';
+            $color = 'danger';
+
+            if($user->isEstado()){
+
+                $estado = 'Alta';
+                $color = 'success';
+
+            }
+
+
+            $label = "<span class='label label-$color btnEstado' idUsuario='".$user->getId()."' estadoUsuario='".$user->isEstado()."'>$estado</span>";
+
+            $datosJson .='[
+					"'.$user->getNombre().'",
+					"'.$user->getApellidos().'",
+					"2022-01-01",
+					"'.$sexo.'",
+					"'.$label.'",
+					"'.$btns.'"],';
+
+        }
+
+        $datosJson = substr($datosJson, 0, -1);
+
+        $datosJson .=   '] 
+
+		 }';
+
+        echo $datosJson;
+
+    }
+
 }
